a56c5336ccf1a1c521ff2bbe30db4e38f895d893
Thien <dvkndn@gmail.com>
2020-05-09 20:58:40 +0700

    build: copy theme scss utils to dist

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -1,8 +1,13 @@
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
+import copy from "rollup-plugin-copy";
 
 const isKnownIssue = ({ code, id }) =>
 	(code === "THIS_IS_UNDEFINED" && id.includes("focus-visible")) || false;
+
+const copyOpts = {
+	targets: [{ src: "src/theme", dest: "dist/" }],
+};
 
 export default {
 	input: "src/index.ts",
@@ -11,11 +16,9 @@ export default {
 		format: "es",
 	},
 	external: ["react"],
-	plugins: [postcss(), typescript()],
+	plugins: [postcss(), typescript(), copy(copyOpts)],
 	onwarn: (warning, warn) => {
-		if (isKnownIssue) {
-			return;
-		}
+		if (isKnownIssue) return;
 		warn(warning);
 	},
 };
