09de58162c40c2f9aec8a98e0e57beb09e54020d
Thien <dvkndn@gmail.com>
2020-05-14 03:13:54 +0700

    feat(scrollbar): initial implementation

---
+++ core/src/scrollbar/scrollbar.ts
@@ -0,0 +1,8 @@
+import s from "./scrollbar.module.scss";
+import { validateStyles } from "../utils/utils";
+
+const { custom } = s;
+
+export const scrollbar = { custom };
+
+validateStyles(scrollbar);
