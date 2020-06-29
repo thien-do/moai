0544f015c5055bdcfeec40a2e8d7405419a76520
Thien <dvkndn@gmail.com>
2020-06-29 15:25:10 +0700

    feat: button add disabled style

--- core/src/button/button.tsx
+++ core/src/button/button.tsx
@@ -11,6 +11,7 @@ import outset from "./outset.module.scss";
 export interface ButtonStyle {
 	main: string;
 	selected: string;
+	disabled: string;
 	highlight: string;
 }
 
@@ -19,40 +20,45 @@ export type ButtonSize = {
 	iconSize: IconSize;
 };
 
-interface VisualProps {
-	selected: boolean;
-	highlight: boolean;
-	style: ButtonStyle;
-	size: ButtonSize;
-	isFullWidth: boolean;
-}
-
-const getClass = (props: VisualProps) => {
-    const { highlight, selected, size, style, isFullWidth } = props;
+const getClass = (props: Props) => {
+	const { highlight, selected, size, style, isFullWidth, disabled } = props;
 	if (highlight === true && selected === true)
 		throw Error("Button cannot have both highlight and selected (yet).");
 	const classes = [s.button, size.main, style.main];
 	if (isFullWidth) classes.push(s.fullWidth);
 	if (selected) classes.push(style.selected);
 	if (highlight) classes.push(style.highlight);
+	if (disabled) classes.push(style.disabled, s.disabled);
 	return classes.join(" ");
 };
 
-interface Props extends VisualProps {
+interface Props {
 	onClick: () => void;
 	children?: React.ReactNode;
 	icon?: IconPath;
+	disabled?: boolean;
+	// visual
+	selected?: boolean;
+	highlight?: boolean;
+	isFullWidth?: boolean;
+	// visual with default
+	style: ButtonStyle;
+	size: ButtonSize;
 }
 
-export const Button = ({ icon, children, onClick, ...style }: Props) => (
-	<button onClick={onClick} className={getClass(style)}>
-		{icon && (
+export const Button = (props: Props) => (
+	<button
+		onClick={props.onClick}
+		className={getClass(props)}
+		disabled={props.disabled}
+	>
+		{props.icon && (
 			<span className={s.icon}>
-				<Icon size={style.size.iconSize} path={icon} />
+				<Icon size={props.size.iconSize} path={props.icon} />
 			</span>
 		)}
-		{icon && children && <DivPx size={8} />}
-		{children && <span className={s.text}>{children}</span>}
+		{props.icon && props.children && <DivPx size={8} />}
+		{props.children && <span className={s.text}>{props.children}</span>}
 	</button>
 );
 
@@ -61,11 +67,13 @@ Button.style = {
 		main: `${s.outset} ${outset.main} ${outline.outer}`,
 		selected: outset.selected,
 		highlight: outset.highlight,
+		disabled: outset.disabled,
 	} as ButtonStyle,
 	flat: {
 		main: `${flat.main} ${outline.inner}`,
 		selected: flat.selected,
 		highlight: "",
+		disabled: "",
 	} as ButtonStyle,
 };
 
@@ -81,9 +89,6 @@ Button.size = {
 };
 
 Button.defaultProps = {
-	selected: false,
-	highlight: false,
 	style: Button.style.outset,
 	size: Button.size.medium,
-	isFullWidth: false,
 };
