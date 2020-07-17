029a622b1f7ad7b16d3dd9f5a1a4d27b341fddb8
Thien <dvkndn@gmail.com>
2020-07-17 13:31:00 +0700

    refactor: export div size type

--- core/src/div/div.tsx
+++ core/src/div/div.tsx
@@ -4,9 +4,9 @@ import s from "./div.module.scss";
 
 export const DivEm = () => <span> </span>;
 
-type Size = 4 | 8 | 12 | 16 | 20 | 24 | 32 | 48 | 64 | 96;
+export type DivSize = 4 | 8 | 12 | 16 | 20 | 24 | 32 | 48 | 64 | 96;
 
-export const DivPx = ({ size }: { size: Size }) => (
+export const DivPx = ({ size }: { size: DivSize }) => (
 	<span className={s.px} style={{ width: size, height: size }} />
 );
 
