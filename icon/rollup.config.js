b9477784d16b4f18f0a8002e30a80cc6c354a654
Thien <dvkndn@gmail.com>
2020-08-05 15:23:13 +0700

    feat: add types for icon

--- icon/rollup.config.js
+++ icon/rollup.config.js
@@ -1,10 +1,21 @@
+import typescript from "@rollup/plugin-typescript";
+
+/** @type {import("@rollup/plugin-typescript").RollupTypescriptOptions} */
+const typescriptOptions = {
+	// These options cannot be defined in tsconfig.json
+	// - https://github.com/rollup/plugins/issues/61#issuecomment-597090769
+	declaration: true,
+	declarationDir: "dist/types",
+	// Known issue: "rootDir" is unnecessarily required for declaration
+	// - https://github.com/rollup/plugins/issues/61#issuecomment-596270901
+	rootDir: "src",
+};
+
 /** @type {import("rollup").RollupOptions} */
 const options = {
-	input: "src/index.js",
-	output: [
-		{ file: "dist/index.es.js", format: "es" },
-		{ file: "dist/index.cjs.js", format: "cjs" },
-	],
+	input: "src/index.ts",
+	output: [{ dir: "dist", format: "es" }],
+	plugins: [typescript(typescriptOptions)],
 };
 
 export default options;
