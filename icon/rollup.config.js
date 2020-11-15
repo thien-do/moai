a45610bd03a552688b0aa91ba7320830b3c3bcc1
dvkndn <dvkndn@gmail.com>
2020-11-15 18:05:15 +0700

    Feat(icon): Distribute cjs

--- icon/rollup.config.js
+++ icon/rollup.config.js
@@ -1,21 +1,13 @@
 import typescript from "@rollup/plugin-typescript";
 
-/** @type {import("@rollup/plugin-typescript").RollupTypescriptOptions} */
-const typescriptOptions = {
-	// These options cannot be defined in tsconfig.json
-	// - https://github.com/rollup/plugins/issues/61#issuecomment-597090769
-	declaration: true,
-	declarationDir: "dist/types",
-	// Known issue: "rootDir" is unnecessarily required for declaration
-	// - https://github.com/rollup/plugins/issues/61#issuecomment-596270901
-	rootDir: "src",
-};
-
 /** @type {import("rollup").RollupOptions} */
 const options = {
 	input: "src/index.ts",
-	output: [{ dir: "dist", format: "es" }],
-	plugins: [typescript(typescriptOptions)],
+	output: [
+		{ dir: "dist/esm", format: "esm" },
+		{ dir: "dist/cjs", format: "cjs" },
+	],
+	plugins: [typescript()],
 };
 
 export default options;
---
+++ icon/scripts/release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+version=$(npm version minor)
+git add ./package.json ./package-lock.json
+git commit -m "Build(icon): Release $version"
+git tag -a v$version -m "Release $version"
+npm publish
