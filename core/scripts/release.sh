e942c9c64a676e23cd05ff6c744aa5c9ced46a7b
dvkndn <dvkndn@gmail.com>
2020-11-25 11:38:25 +0700

    Build(all): Avoid generating tag

--- core/scripts/release.sh
+++ core/scripts/release.sh
@@ -6,5 +6,4 @@ npm run build
 version=$(npm version minor)
 git add ./package.json ./package-lock.json
 git commit -m "Build(core): Release $version"
-git tag -a v$version -m "Release $version"
 npm publish
