2335888509c002632111b5654903a976a483a177
dvkndn <dvkndn@gmail.com>
2020-12-13 23:35:16 +0700

    Docs(core): Update tab stories

--- core/src/tab/tab.stories.tsx
+++ core/src/tab/tab.stories.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Tabs } from "./tab";
 
 storiesOf("Tab", module).add("Primary", () => (
-	<Tabs>
-		{[
-			{ title: "First", pane: () => <p>First pane</p> },
-			{ title: "Second", pane: () => <p>Second pane</p> },
+	<Tabs
+		children={[
+			{ id: "First", title: "First", pane: () => <p>First pane</p> },
+			{ id: "Second", title: "Second", pane: () => <p>Second pane</p> },
 		]}
-	</Tabs>
+	/>
 ));
