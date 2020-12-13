e07f600b10678e98c52c7682a5828fd3e0526d78
dvkndn <dvkndn@gmail.com>
2020-12-13 23:32:27 +0700

    Feat(core): Allow users to set initial tab
    Also require "id" to identify each tab

--- core/src/tab/tab.tsx
+++ core/src/tab/tab.tsx
@@ -6,44 +6,51 @@ import { Pane } from "../pane/pane";
 import s from "./tab.module.css";
 
 export interface Tab {
+	id: string;
 	title: string;
 	pane: () => JSX.Element;
 }
 
 interface Props {
 	children: Tab[];
+	initialTab?: string;
 	noPadding?: boolean;
 }
 
-type State = [string, (str: string) => void];
+interface State {
+	active: string;
+	setActive: (str: string) => void;
+}
 
-const renderTab = (state: State) => (tab: Tab) => {
-	const [active, setActive] = state;
+const renderTitle = (state: State) => (tab: Tab) => {
+	const { active, setActive } = state;
 	const activeCls =
-		active === tab.title
+		active === tab.id
 			? `${borderColor.weak} ${background.primary}`
 			: `${s.nonactive}`;
-	const className = [s.title, outline.normal, activeCls].join(" ");
-	const onClick = () => setActive(tab.title);
 	return (
-		<button key={tab.title} {...{ className, onClick }}>
-			{tab.title}
-		</button>
+		<button
+			className={[s.title, outline.normal, activeCls].join(" ")}
+			onClick={() => setActive(tab.id)}
+			key={tab.id}
+			children={tab.title}
+		/>
 	);
 };
 
 export const Tabs = ({ children, ...props }: Props): JSX.Element => {
 	if (children.length < 1) throw Error("Tabs must have at least one tab");
-	const activeState = React.useState(children[0].title);
-	const [active] = activeState;
+	const [active, setActive] = React.useState(
+		props.initialTab ?? children[0].id
+	);
 
-	const activeTab = children.find((tab) => tab.title === active);
+	const activeTab = children.find((tab) => tab.id === active);
 	if (activeTab === undefined) throw Error(`Tab "${active}" is not defined`);
 
 	return (
 		<div>
 			<div className={s.titles}>
-				{children.map(renderTab(activeState))}
+				{children.map(renderTitle({ active, setActive }))}
 			</div>
 			<Pane noPadding={props.noPadding}>
 				<div className={s.panel}>{activeTab.pane()}</div>
