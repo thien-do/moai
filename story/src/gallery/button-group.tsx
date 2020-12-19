09d77987c3f4a59c366d8548b0a12fd0684b09c3
dvkndn <dvkndn@gmail.com>
2020-12-19 21:47:36 +0700

    Fix(story): Re group button stories

--- story/src/gallery/button-group.tsx
+++ story/src/gallery/button-group.tsx
@@ -1,53 +1,38 @@
 import { useState } from "react";
-import { Button, ButtonGroup } from "../../../core/src";
+import { Button, ButtonGroup, ButtonProps, Switcher } from "../../../core/src";
 import { icons } from "../../../icon/src";
 
-const Theme = () => {
-	const [value, setValue] = useState("Auto");
-	return (
-		<ButtonGroup>
-			{["Light", "Auto", "Dark"].map((id) => (
-				<Button
-					key={id}
-					children={id}
-					selected={value === id}
-					onClick={() => setValue(id)}
-				/>
-			))}
-		</ButtonGroup>
-	);
-};
+const { medium, small } = Button.size;
+const { flat } = Button.style;
+const icon = icons.plus;
 
-const Toggle = (): JSX.Element => {
-	const [selected, setSelected] = useState(false);
-	return (
-		<Button
-			children="Toggle"
-			selected={selected}
-			onClick={() => setSelected(!selected)}
-		/>
-	);
-};
+const flatIcon: ButtonProps = { icon, style: flat };
 
 export const GalleryButtonGroup = (): JSX.Element => (
 	<div className="space-y-8">
 		<div className="flex space-x-8">
-			<Toggle />
-			<Theme />
-		</div>
-		<div className="flex space-x-8">
 			<Button children="Back" icon={icons.arrowLeft} />
 			<Button children="Next" icon={icons.arrowRight} reverse />
-			<Button iconLabel="Search" icon={icons.search} />
 		</div>
-		<div>
+		<div className="flex space-x-8">
 			<ButtonGroup>
 				<Button icon={icons.plus} children="Add" />
 				<Button icon={icons.caret} iconLabel="More" />
 			</ButtonGroup>
+			<Button iconLabel="Search" icon={icons.search} />
+			<Button iconLabel="Search" icon={icons.search} disabled />
 		</div>
-		<div>
-			<Button fill children="Filled Button" />
+		<div className="flex space-x-8">
+			<Button icon={icon} size={medium} children="Medium" />
+			<Button icon={icon} size={small} children="Small" />
+			<Button icon={icon} size={small} iconLabel="Add" />
+			<Button icon={icon} size={small} iconLabel="Add" busy />
+		</div>
+		<div className="flex space-x-8">
+			<Button {...flatIcon} size={medium} children="Medium" />
+			<Button {...flatIcon} size={small} children="Small" />
+			<Button {...flatIcon} size={small} iconLabel="Add" />
+			<Button {...flatIcon} size={small} iconLabel="Add" busy />
 		</div>
 	</div>
 );
--- story/src/gallery/button-size.tsx
+++ story/src/gallery/button-size.tsx
@@ -1,32 +1,66 @@
-import { Button, ButtonProps } from "../../../core/src";
-import { icons } from "../../../icon/src";
+import { useState } from "react";
+import { Button, Switcher } from "../../../core/src";
 
-const { medium, small } = Button.size;
-const { flat } = Button.style;
-const icon = icons.plus;
+const Theme = () => {
+	const [value, setValue] = useState("Auto");
+	return (
+		<Switcher
+			options={["Light", "Auto", "Dark", "Disabled"].map((id) => ({
+				value: id,
+				label: id,
+				disabled: id === "Disabled",
+			}))}
+			value={value}
+			setValue={setValue}
+		/>
+	);
+};
 
-const flatIcon: ButtonProps = { icon, style: flat };
+const Busy = (): JSX.Element => {
+	const [busy, setBusy] = useState(false);
+	return (
+		<Button
+			children="Click to Load"
+			busy={busy}
+			onClick={() => {
+				setBusy(true);
+				window.setTimeout(() => {
+					setBusy(false);
+				}, 1000);
+			}}
+		/>
+	);
+};
+
+const Toggle = (): JSX.Element => {
+	const [selected, setSelected] = useState(false);
+	return (
+		<Button
+			children="Click to Toggle"
+			selected={selected}
+			onClick={() => setSelected(!selected)}
+		/>
+	);
+};
 
 export const GalleryButtonSize = (): JSX.Element => (
 	<div className="space-y-8">
 		<div className="flex space-x-8">
-			<Button icon={icon} size={medium} children="Medium" />
-			<Button icon={icon} size={small} children="Small" />
-			<Button icon={icon} size={small} iconLabel="Add" />
-			<Button icon={icon} size={small} iconLabel="Add" busy />
+			<Toggle />
+			<Busy />
 		</div>
 		<div className="flex space-x-8">
-			<Button {...flatIcon} size={medium} children="Medium" />
-			<Button {...flatIcon} size={small} children="Small" />
-			<Button {...flatIcon} size={small} iconLabel="Add" />
-			<Button {...flatIcon} size={small} iconLabel="Add" busy />
-		</div>
-		<div>
 			<Button
 				target="_blank"
 				href="https://moai.xyz"
 				children="Link to moai.xyz"
 			/>
+		</div>
+		<div>
+			<Button fill children="Full-width Button" />
+		</div>
+		<div className="flex space-x-8">
+			<Theme />
 		</div>
 	</div>
 );
