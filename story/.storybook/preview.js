46144994e61da4f6bd637ab97fa406b1898065ee
dvkndn <dvkndn@gmail.com>
2020-12-20 17:49:56 +0700

    Feat(story): Add common backgrounds

--- story/.storybook/preview.js
+++ story/.storybook/preview.js
@@ -11,6 +11,12 @@ export const parameters = {
 			opacity: 0.2,
 			cellAmount: 0,
 		},
+		values: [
+			{ name: "gray-0 (Light default)", value: "#ffffff" },
+			{ name: "gray-1 (Light dimmed)", value: "#fafafa" },
+			{ name: "gray-8 (Dark default)", value: "#34353c" },
+			{ name: "gray-9 (Dark dimmed)", value: "#27272a" },
+		],
 	},
 	darkMode: {
 		stylePreview: true,
