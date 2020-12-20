55f1e7a0252cedee05547c038bb409a1f0039909
dvkndn <dvkndn@gmail.com>
2020-12-20 23:25:27 +0700

    Feat(core): Expose IconProps

--- core/src/icon/icon.tsx
+++ core/src/icon/icon.tsx
@@ -3,13 +3,13 @@ import s from "./icon.module.css";
 export type IconSize = 12 | 16 | 36;
 export type IconPath = string;
 
-interface Props {
+export interface IconProps {
 	path: IconPath;
 	size?: IconSize;
 	display: "block" | "inline";
 }
 
-export const Icon = ({ path, size, display }: Props): JSX.Element => (
+export const Icon = ({ path, size, display }: IconProps): JSX.Element => (
 	<svg
 		className={[s.icon, display === "block" ? s.block : s.inline].join(" ")}
 		xmlns="http://www.w3.org/2000/svg"
