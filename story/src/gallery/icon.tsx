b0bff2e17ac054748404447b20201f993e1ed7cb
dvkndn <dvkndn@gmail.com>
2020-12-22 00:42:37 +0700

    Fix(story): Remove tooltip in Icon section due to performance issue

--- story/src/gallery/icon.tsx
+++ story/src/gallery/icon.tsx
@@ -1,16 +1,14 @@
-import { Icon, Tooltip } from "../../../core/src";
+import { Icon } from "../../../core/src";
 import { icons } from "../../../icon/src";
 
 const IconBox = ({ name }: { name: string }): JSX.Element => (
-	<Tooltip content={name} placement="bottom">
-		<div
-			className="py-16 px-8 flex flex-col items-center space-y-8 text-center"
-			style={{ width: 96 }}
-		>
-			<Icon display="block" path={icons[name]} />
-			<div className="truncate w-full">{name}</div>
-		</div>
-	</Tooltip>
+	<div
+		className="py-16 px-8 flex flex-col items-center space-y-8 text-center"
+		style={{ width: 96 }}
+	>
+		<Icon display="block" path={(icons as any)[name]} />
+		<div className="truncate w-full">{name}</div>
+	</div>
 );
 
 export const GalleryIcon = () => (
