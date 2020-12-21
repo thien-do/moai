5450c5eec2f7254cc9b107af1d25bd59e9c4328b
dvkndn <dvkndn@gmail.com>
2020-12-22 05:00:14 +0700

    Refactor(site): Extract sample data

---
+++ site/components/gallery/samples/people.ts
@@ -0,0 +1,12 @@
+export interface Person {
+	id: string;
+	first_name: string;
+	last_name: string;
+	email: string;
+	phone: string;
+	note: string;
+	avatar: string;
+	genres: string;
+	color: string;
+	deployed: number;
+}
--- story/tailwind.config.js
+++ story/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
 		spacing: {
 			"4": "4px",
 			"8": "8px",
+			"12": "12px",
 			"16": "16px",
 			"32": "32px",
 		},
