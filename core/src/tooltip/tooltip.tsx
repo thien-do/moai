133f4204afee5bbe96122126cea7baab5f20ad0b
dvkndn <dvkndn@gmail.com>
2020-12-22 00:42:01 +0700

    Feat(core): Add Tooltip pane

--- core/src/tooltip/tooltip.tsx
+++ core/src/tooltip/tooltip.tsx
@@ -1,5 +1,4 @@
 import Tippy, { TippyProps } from "@tippyjs/react/headless";
-import React from "react";
 import { background } from "../background/background";
 import { boxShadow } from "../box-shadow/box-shadow";
 import { Paragraph } from "../text/text";
@@ -11,25 +10,32 @@ export interface TooltipProps {
 	placement?: TippyProps["placement"];
 }
 
+interface PaneProps {
+	children: TooltipProps["content"];
+	attrs?: object;
+}
+
+export const TooltipPane = (props: PaneProps): JSX.Element => (
+	<div
+		className={[s.content, background.inverse, boxShadow.strong].join(" ")}
+		tabIndex={-1}
+		{...props.attrs}
+	>
+		{props.attrs && (
+			<div data-popper-arrow className={s.arrowWrapper}>
+				<div className={[s.arrow, background.inverse].join(" ")} />
+			</div>
+		)}
+		<Paragraph children={props.children} />
+	</div>
+);
+
 export const Tooltip = (props: TooltipProps): JSX.Element => (
 	<Tippy
 		children={props.children}
-		placement={props.placement}
+		placement={props.placement ?? "auto"}
 		render={(attrs) => (
-			<div
-				className={[
-					s.content,
-					background.inverse,
-					boxShadow.strong,
-				].join(" ")}
-				tabIndex={-1}
-				{...attrs}
-			>
-				<div data-popper-arrow className={s.arrowWrapper}>
-					<div className={[s.arrow, background.inverse].join(" ")} />
-				</div>
-				<Paragraph children={props.content} />
-			</div>
+			<TooltipPane children={props.content} attrs={attrs} />
 		)}
 	/>
 );
