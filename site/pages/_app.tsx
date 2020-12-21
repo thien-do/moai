5a16b7fcaf1077d7304eb523ecaa054ff850c3f1
dvkndn <dvkndn@gmail.com>
2020-12-22 04:59:26 +0700

    Fix(site): Use inline SVG for Favicon

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -4,11 +4,17 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import "tailwindcss/utilities.css";
 
+const favIcon: string = [
+	"data:image/svg+xml,",
+	"<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>",
+	"<text y=%220.9em%22 font-size=%2290%22>🗿</text></svg>",
+].join("");
+
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
 	<>
 		<Head>
 			<title>Moai UI Kit</title>
-			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			<link rel="icon" type="image/svg+xml" href={favIcon} />
 		</Head>
 		<Component {...pageProps} />
 	</>
