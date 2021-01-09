a527bc69f9d789f5da9c295b574c82bfbe45fc4d
Thien <dvkndn@gmail.com>
2021-01-09 10:55:07 +0700

    Docs(core): Avoid using Tailwind in Tag stories

--- core/src/tag/tag.stories.tsx
+++ core/src/tag/tag.stories.tsx
@@ -1,11 +1,15 @@
 import { storiesOf } from "@storybook/react";
+import { DivPx } from "../div/div";
 import { Tag } from "./tag";
 
 storiesOf("Tag", module).add("Main", () => (
-	<div className="space-x-4">
+	<div style={{ display: "flex" }}>
 		<Tag type={Tag.types.neutral}>Neutral</Tag>
+		<DivPx size={8} />
 		<Tag type={Tag.types.success}>Success</Tag>
+		<DivPx size={8} />
 		<Tag type={Tag.types.highlight}>Highlight</Tag>
+		<DivPx size={8} />
 		<Tag type={Tag.types.failure}>Failure</Tag>
 	</div>
 ));
