706dc4d2ddc034f8cd7010bfc5374cbc963d60aa
Thien <dvkndn@gmail.com>
2021-01-12 13:43:55 +0700

    Fix(gallery): Avoid using coreIcons at init time

--- gallery/src/widget/button-3.tsx
+++ gallery/src/widget/button-3.tsx
@@ -3,9 +3,9 @@ import { Button, ButtonGroup, ButtonProps, coreIcons } from "@moai/core";
 const { medium, small } = Button.sizes;
 const { flat } = Button.styles;
 
-const Plus = coreIcons.plus;
+const Plus = () => coreIcons.plus;
 
-const flatIcon: ButtonProps = { icon: Plus, style: flat };
+const flatIcon: ButtonProps = { icon: Plus(), style: flat };
 
 export const Button3Gallery = (): JSX.Element => (
 	<div className="space-y-8">
@@ -17,19 +17,19 @@ export const Button3Gallery = (): JSX.Element => (
 		</div>
 		<div className="flex space-x-8">
 			<ButtonGroup>
-				<Button icon={Plus} children="Add" />
+				<Button icon={Plus()} children="Add" />
 				<Button icon={coreIcons.caret} iconLabel="More" />
 			</ButtonGroup>
 			<ButtonGroup>
-				<Button disabled icon={Plus} children="Add" />
+				<Button disabled icon={Plus()} children="Add" />
 				<Button disabled icon={coreIcons.caret} iconLabel="More" />
 			</ButtonGroup>
 		</div>
 		<div className="flex space-x-8">
-			<Button icon={Plus} size={medium} children="Medium" />
-			<Button icon={Plus} size={small} children="Small" />
-			<Button icon={Plus} size={small} iconLabel="Add" />
-			<Button icon={Plus} size={small} iconLabel="Add" busy />
+			<Button icon={Plus()} size={medium} children="Medium" />
+			<Button icon={Plus()} size={small} children="Small" />
+			<Button icon={Plus()} size={small} iconLabel="Add" />
+			<Button icon={Plus()} size={small} iconLabel="Add" busy />
 		</div>
 		<div className="flex space-x-8">
 			<Button {...flatIcon} size={medium} children="Medium" />
