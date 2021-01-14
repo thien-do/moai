55cfee23224eb9fc3fb351aa74e9a60f73d3112f
Thien <dvkndn@gmail.com>
2021-01-14 21:36:51 +0700

    Feat(core): Support full height in Pane

--- core/src/pane/pane.stories.tsx
+++ core/src/pane/pane.stories.tsx
@@ -1,9 +1,16 @@
 import { storiesOf } from "@storybook/react"; //eslint-disable-line
 import React from "react";
+import { DivPx } from "../div/div";
 import { Pane } from "./pane";
 
-storiesOf("Pane", module).add("Default", () => <Pane>Content</Pane>);
-
-storiesOf("Pane", module).add("No padding", () => (
-	<Pane noPadding>Content</Pane>
+storiesOf("Pane", module).add("Default", () => (
+	<div>
+		<Pane>Content</Pane>
+		<DivPx size={16} />
+		<Pane noPadding>Content</Pane>
+		<DivPx size={16} />
+		<div style={{ height: 100 }}>
+			<Pane fullHeight>Content</Pane>
+		</div>
+	</div>
 ));
--- core/src/pane/pane.tsx
+++ core/src/pane/pane.tsx
@@ -1,15 +1,19 @@
-import * as React from "react";
 import s from "./pane.module.css";
 import { pane } from "./style";
 
 interface Props {
 	children: React.ReactNode;
 	noPadding?: boolean;
+	fullHeight?: boolean;
 }
 
-export const Pane = ({ children, noPadding }: Props): JSX.Element => (
+export const Pane = (props: Props): JSX.Element => (
 	<div
-		className={[pane.outset, noPadding ? "" : s.padding].join(" ")}
-		children={children}
+		className={[
+			pane.outset,
+			props.noPadding ? "" : s.padding,
+			props.fullHeight ? s.fullHeight : "",
+		].join(" ")}
+		children={props.children}
 	/>
 );
