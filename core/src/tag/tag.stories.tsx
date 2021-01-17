743a7157921061a713ec70dafc544ce4d5db1959
Thien <dvkndn@gmail.com>
2021-01-17 15:08:33 +0700

    Fix(core): Limit Tag height to 16px

--- core/src/tag/tag.stories.tsx
+++ core/src/tag/tag.stories.tsx
@@ -3,13 +3,20 @@ import { DivPx } from "../div/div";
 import { Tag } from "./tag";
 
 storiesOf("Tag", module).add("Main", () => (
-	<div style={{ display: "flex" }}>
-		<Tag type={Tag.types.neutral}>Neutral</Tag>
-		<DivPx size={8} />
-		<Tag type={Tag.types.success}>Success</Tag>
-		<DivPx size={8} />
-		<Tag type={Tag.types.highlight}>Highlight</Tag>
-		<DivPx size={8} />
-		<Tag type={Tag.types.failure}>Failure</Tag>
+	<div>
+		<div style={{ display: "flex" }}>
+			<Tag type={Tag.types.neutral}>Neutral</Tag>
+			<DivPx size={8} />
+			<Tag type={Tag.types.success}>Success</Tag>
+			<DivPx size={8} />
+			<Tag type={Tag.types.highlight}>Highlight</Tag>
+			<DivPx size={8} />
+			<Tag type={Tag.types.failure}>Failure</Tag>
+		</div>
+		<div>
+			<span>
+				<Tag type={Tag.types.neutral}>Test height</Tag>
+			</span>
+		</div>
 	</div>
 ));
