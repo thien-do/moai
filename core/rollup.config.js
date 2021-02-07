0b709c6c64f8f5e8a8ad94bcda316660f87e89d2
Thien <dvkndn@gmail.com>
2021-02-08 00:07:42 +0700

    Build(core): Distribute from root

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -1,8 +1,6 @@
-import replace from "@rollup/plugin-replace";
 import cssPrefix from "autoprefixer";
 import copy from "rollup-plugin-copy";
 import del from "rollup-plugin-delete";
-import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import typescript from "rollup-plugin-typescript2";
 import fs from "fs";
@@ -50,43 +48,17 @@ const bundleMain = (() => {
 })();
 
 /**
- * Declaration bundling process
- *
- * Bundling declarations manually because TS doesn't support this yet
- * See: https://github.com/microsoft/TypeScript/issues/4433
- */
-const bundleDts = (() => {
-	/**
-	 * Not really sure why "import index.css" is left in index.d.ts. It's not
-	 * necessary but making the DTS plugin to fail. We simply need to remove it.
-	 *
-	 * @type {import("@rollup/plugin-replace").RollupReplaceOptions}
-	 */
-	const removeCSS = { 'import "./style/style";': "", delimiters: ["", ""] };
-
-	/** @type {import("rollup").RollupOptions} */
-	const options = {
-		input: "./dist/types/index.d.ts",
-		output: [{ file: "./dist/index.d.ts" }],
-		plugins: [replace(removeCSS), dts()],
-	};
-
-	return options;
-})();
-
-/**
- * Static bundling process. Basically re-organizing the generated distribution.
+ * Static bundling process
  */
 const bundleStatic = (() => {
 	/** @type {import("rollup-plugin-copy").Target[]} */
 	const copyTargets = [
-		{ src: "./package.json", dest: "./dist" },
 		{ src: "./font", dest: "./dist" },
 		{ src: "./dist/cjs.css", dest: "./dist", rename: "index.css" },
 	];
 
 	/** @type {import("rollup-plugin-delete").Options["targets"]} */
-	const deleteTargets = ["./dist/cjs.css", "./dist/esm.css", "./dist/types"];
+	const deleteTargets = ["./dist/cjs.css", "./dist/esm.css"];
 
 	/**
 	 * A custom plugin to throw away the main (dummy) output of this process.
