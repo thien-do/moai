8556d2508fee908f55a53d326a9c88988d67e389
Thien <dvkndn@gmail.com>
2021-02-09 07:57:39 +0700

    Refactor(core): Reverse color changes

--- core/src/tooltip/tooltip.stories.tsx
+++ core/src/tooltip/tooltip.stories.tsx
@@ -1,16 +1,36 @@
-import { storiesOf } from "@storybook/react";
-import { Button } from "../button/button";
-import { DivPx } from "../div/div";
-import { Tooltip, TooltipPane } from "./tooltip";
+// YourComponent.stories.tsx
 
-storiesOf("Tooltip", module).add("Main", () => (
-	<div>
-		<Tooltip content="Sample Tooltip">
-			<Button.Forwarded children="Hover to show a Tooltip" />
-		</Tooltip>
-		<DivPx size={16} />
-		<TooltipPane children="Short Tooltip" />
-		<DivPx size={16} />
-		<TooltipPane children="Multi-line Tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit" />
-	</div>
-));
+import React, { ComponentProps } from 'react';
+import { Story } from '@storybook/react/types-6-0';
+
+import { Tooltip } from './tooltip';
+
+// This default export determines where your story goes in the story list
+export default {
+  title: 'Tooltip',
+  component: Tooltip,
+  argTypes: {
+    children: {
+      name: 'children',
+      type: { name: 'object', required: true },
+      defaultValue: <a href="#">Test</a>,
+      description: 'children',
+      table: {
+        type: { summary: 'nodeElement' },
+        defaultValue: { summary: 'Hello' },
+      },
+      control: {
+        type: 'object'
+      }
+    },
+  }
+};
+
+const Template: Story<ComponentProps<typeof Tooltip>> = (args) => (
+  <Tooltip {...args}>{args.children}</Tooltip>
+);
+
+export const FirstStory = Template.bind({});
+FirstStory.args = {
+  content: "This is test"
+};
