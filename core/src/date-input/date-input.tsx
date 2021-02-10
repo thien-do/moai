6d8d903b1bf8518b00e507bd0065817ff362dcf3
Thien <dvkndn@gmail.com>
2021-02-11 03:19:33 +0700

    Feat(core): Add state controller to DateInput

--- core/src/date-input/date-input.tsx
+++ core/src/date-input/date-input.tsx
@@ -1,12 +1,19 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, RefObject, useState } from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
-import "./date-input.css";
-import { Input } from "../input/input";
+import { Input, InputProps } from "../input/input";
 import { PopoverPane } from "../popover/pane/pane";
+import "./date-input.css";
 import { DateInputMonth } from "./month/month";
 import { DateInputNavbar } from "./navbar/navbar";
 
-interface Props {}
+interface Props {
+	value?: Date | null;
+	setValue?: (date: Date | null) => void;
+	initialValue?: Date | null;
+	size?: InputProps["size"];
+	style?: InputProps["style"];
+	forwardedRef?: RefObject<DayPickerInput>;
+}
 
 interface DayPickerOverlayProps {
 	children: ReactNode;
@@ -34,12 +41,28 @@ const Overlay = (props: OverlayProps): JSX.Element | null => {
 	);
 };
 
+const getValue = (props: Props): Date | undefined => {
+	// Controlled
+	if (props.value !== undefined) {
+		return props.value ?? undefined;
+	}
+	// Uncontrolled w initial value
+	if (props.initialValue !== undefined) {
+		return props.initialValue ?? undefined;
+	}
+	// Uncontrolled wo initial value
+	return undefined;
+};
+
 export const DateInput = (props: Props): JSX.Element => {
 	const [target, setTarget] = useState<HTMLDivElement | null>(null);
 	const [month, setMonth] = useState<Date>(() => new Date());
+
 	return (
 		<div ref={setTarget}>
 			<DayPickerInput
+				ref={props.forwardedRef}
+				inputProps={{ style: props.style, size: props.size }}
 				component={Input.Forwarded}
 				overlayComponent={(dayPicker: DayPickerOverlayProps) => (
 					<Overlay target={target} {...dayPicker} />
@@ -50,6 +73,12 @@ export const DateInput = (props: Props): JSX.Element => {
 					captionElement: ({ date }) => (
 						<DateInputMonth value={date} setValue={setMonth} />
 					),
+				}}
+				value={getValue(props)}
+				// The lib's type is missing "undefined" case
+				onDayChange={(day: Date | undefined) => {
+					if (props.setValue === undefined) return;
+					props.setValue(day ?? null);
 				}}
 			/>
 		</div>
