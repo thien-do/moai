345273576ca3a87a80bf9e441ccd773fe2398a4f
Thien <dvkndn@gmail.com>
2021-02-11 23:30:42 +0700

    Docs(core): Add DateInput ref example

--- core/src/date-input/date-input.stories.tsx
+++ core/src/date-input/date-input.stories.tsx
@@ -1,5 +1,6 @@
 import { storiesOf } from "@storybook/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import DayPickerInput from "react-day-picker/types/DayPickerInput";
 import { Button } from "../button/button";
 import { DivPx } from "../div/div";
 import { DateInput } from "./date-input";
@@ -8,11 +9,17 @@ const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 
 storiesOf("DateInput", module).add("Main", () => {
-	const [date, setDate] = useState<Date | null>(() => new Date());
+	const [date, setDate] = useState<null | Date>(() => new Date());
+	const ref = useRef<DayPickerInput>(null);
 	return (
 		<div style={{ width: 200 }}>
 			<div>Uncontrolled</div>
-			<DateInput format={DateInput.formats.dmy} />
+			<DateInput format={DateInput.formats.dmy} forwardedRef={ref} />
+			<DivPx size={8} />
+			<Button
+				onClick={() => void window.alert(ref.current.state.value)}
+				children="Get value from ref"
+			/>
 			<DivPx size={16} />
 			<div>Uncontrolled with initial value</div>
 			<DateInput
@@ -20,8 +27,7 @@ storiesOf("DateInput", module).add("Main", () => {
 				initialValue={yesterday}
 			/>
 			<DivPx size={16} />
-			<div>Controlled:</div>
-			<div>Value: {date.toDateString()}</div>
+			<div>Controlled: {date?.toDateString()}</div>
 			<DivPx size={4} />
 			<DateInput
 				format={DateInput.formats.dmy}
