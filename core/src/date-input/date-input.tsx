8aa9ee1d1804f7e94b0f2e97c3f742434b108f3e
Thien <dvkndn@gmail.com>
2021-02-25 18:15:04 +0700

    Docs(core): Improve DateInput doc

--- core/src/date-input/date-input.tsx
+++ core/src/date-input/date-input.tsx
@@ -7,15 +7,23 @@ import "./date-input.css";
 import { DateInputFormat, dateInputFormats } from "./format";
 import { DateInputMonth } from "./month/month";
 import { DateInputNavbar } from "./navbar/navbar";
-import { coreIcons } from '../icons/icons'
 
 interface Props {
 	/**
 	 * Date format of the text box. This is used to display the selected date
 	 * as well as parse the user's input into Date. Choose one from
-	 * `DateInput.formats`.
+	 * `DateInput.formats`. Defaults to "yyyy/mm/dd".
 	 */
-	format: DateInputFormat;
+	format?: DateInputFormat;
+	/**
+	 * The maximum date the user can choose
+	 */
+	maxDate?: Date;
+	/**
+	 * The minimum date the user can choose
+	 */
+	minDate?: Date;
+
 	/**
 	 * Value of the input in controlled mode
 	 */
@@ -29,6 +37,12 @@ interface Props {
 	 */
 	defaultValue?: Date | null;
 	/**
+	 * Whether the input is disabled. This disables the whole input. If you
+	 * want to disable some days, use "minDate" and "maxDate"
+	 */
+	disabled?: InputProps["disabled"];
+
+	/**
 	 * Size of the text box. Choose one from `DateInput.sizes`.
 	 */
 	size?: InputProps["size"];
@@ -36,14 +50,11 @@ interface Props {
 	 * Style of the text box. Choose one from `DateInput.styles`.
 	 */
 	style?: InputProps["style"];
+	/**
+	 * The icon of the text box. See "Icon" page.
+	 */
+	icon?: InputProps["icon"];
 
-	disabled?: InputProps["disabled"];
-
-	maxDate?: Date;
-
-	minDate?: Date;
-
-	icon?: boolean;
 	/**
 	 * Reference to the underlying [DayPickerInput](https://react-day-picker.js.org/api/DayPickerInput/)
 	 * instance.
@@ -90,32 +101,30 @@ const getValue = (props: Props): Date | undefined => {
 	return undefined;
 };
 
+const getDisabledDays = (props: Props): Modifier => {
+	// This is quite complicated because we need to avoid mutating the "days"
+	// variable as well as the strictness of Modifier
+	const { minDate: min, maxDate: max } = props;
+	let days: Modifier = undefined;
+	if (max) days = { after: max };
+	if (min) days = { ...days, before: min };
+	return days;
+};
+
 export const DateInput = (props: Props): JSX.Element => {
 	const [target, setTarget] = useState<HTMLDivElement | null>(null);
 	const [month, setMonth] = useState<Date>(() => new Date());
-
-	let disabledDays: Modifier = undefined
-	if (props.maxDate) {
-		disabledDays = {
-			after: props.maxDate
-		}
-	}
-	if (props.minDate) {
-		disabledDays = {
-			...disabledDays,
-			before: props.minDate
-		}
-	}
+	const format = props.format ?? DateInput.formats.dmy;
 
 	return (
 		<div ref={setTarget}>
 			<DayPickerInput
 				ref={props.forwardedRef}
-				inputProps={{ 
+				inputProps={{
 					style: props.style,
-					size: props.size, 
+					size: props.size,
 					disabled: props.disabled,
-					icon: props.icon ? coreIcons.duplicate : undefined
+					icon: props.icon,
 				}}
 				component={Input.Forwarded}
 				clickUnselectsDay
@@ -128,7 +137,7 @@ export const DateInput = (props: Props): JSX.Element => {
 					captionElement: ({ date }) => (
 						<DateInputMonth value={date} setValue={setMonth} />
 					),
-					disabledDays,
+					disabledDays: getDisabledDays(props),
 				}}
 				value={getValue(props)}
 				// The lib's type is missing "undefined" case
@@ -137,9 +146,9 @@ export const DateInput = (props: Props): JSX.Element => {
 					props.setValue(day ?? null);
 				}}
 				// Format
-				placeholder={props.format.placeholder}
-				formatDate={props.format.format}
-				parseDate={props.format.parse}
+				placeholder={format.placeholder}
+				formatDate={format.format}
+				parseDate={format.parse}
 			/>
 		</div>
 	);
