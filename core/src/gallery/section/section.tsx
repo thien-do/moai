bea1658f60733d833df037e4ffe3662cf0e46f1c
Thien <dvkndn@gmail.com>
2021-02-27 10:25:08 +0700

    Build(core): Move gallery to core

--- core/src/gallery/section/section.tsx
+++ core/src/gallery/section/section.tsx
@@ -1,4 +1,4 @@
-import { DivPx } from "@moai/core";
+import { DivPx } from "../../components";
 import { ReactNode } from "react";
 
 interface Props {
@@ -19,7 +19,7 @@ export const GallerySection = (props: Props): JSX.Element => (
 			className="flex-1 overflow-hidden grid gap-16 p-8 -m-8"
 			style={{
 				gridTemplateColumns: "repeat(auto-fill, min(320px, 100%))",
-				maxWidth: "calc(320px * 3 + 16px * 2 + 8px * 2)"
+				maxWidth: "calc(320px * 3 + 16px * 2 + 8px * 2)",
 			}}
 			children={props.children}
 		/>
