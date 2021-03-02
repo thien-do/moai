b6cf651ef315fe5e8ebc8b60e86ba894b8fd4cf6
Khoa Le <ldakhoa1308@gmail.com>
2021-03-02 20:59:25 +0700

    Docs(core): Fix TextArea doc

--- core/src/components/text-area/text-area.stories.tsx
+++ core/src/components/text-area/text-area.stories.tsx
@@ -1,22 +1,49 @@
 import { _Story } from "../../_story";
-import { DivPx } from "../div/div";
 import { TextArea } from "./text-area";
 
 export default {
 	title: "Components/TextArea",
 	component: TextArea,
+	argTypes: {
+		style: _Story.arg(TextArea.styles),
+		size: _Story.arg(TextArea.sizes),
+
+		disabled: _Story.arg("boolean"),
+		readOnly: _Story.arg("boolean"),
+		rows: _Story.arg(null),
+
+		defaultValue: _Story.arg(null),
+		value: _Story.arg(null),
+		forwardedRef: _Story.arg(null),
+		setValue: _Story.arg(null),
+		placeholder: _Story.arg(null),
+		autoFocus: _Story.arg(null),
+		autoSelect: _Story.arg(null),
+
+		onBlur: _Story.arg(null),
+		onFocus: _Story.arg(null),
+		onKeyPress: _Story.arg(null),
+		onKeyUp: _Story.arg(null),
+		onKeyDown: _Story.arg(null),
+	},
 };
 
-export const Primary = () => {
+interface Props {
+	style?: string;
+	size?: string;
+	disabled?: boolean;
+	readOnly?: boolean;
+}
+
+export const Primary = (props: Props) => {
 	return (
-		<div style={{ width: 300 }}>
-			<TextArea />
-			<DivPx size={8} />
-			<TextArea placeholder="Placeholder" />
-			<DivPx size={8} />
-			<TextArea readOnly value="Read-only" />
-			<DivPx size={8} />
-			<TextArea disabled />
+		<div>
+			<TextArea
+				style={TextArea.styles[props.style]}
+				size={TextArea.sizes[props.size]}
+				disabled={props.disabled}
+				readOnly={props.readOnly}
+			/>
 		</div>
 	);
 };
