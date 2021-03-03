08fbcd3f7a172c9150b6886ee92ac692dd23f9e6
Thien Do <hello@thien.do>
2021-03-03 14:56:35 +0700

    Release(core): v0.223.0

---
+++ core/scripts/report.js
@@ -0,0 +1,17 @@
+const cp = require("child_process");
+
+const files = [
+	"esm.js",
+	"cjs.js",
+	"bundle.css",
+	"_gallery/esm.js",
+	"_gallery/cjs.js",
+	"_gallery/bundle.css",
+];
+
+files.forEach((file) => {
+	const command = `gzip -c dist/${file} | wc -c`;
+	let size = cp.execSync(command).toString();
+	size = size.replace("\n", "");
+	console.log(`${size} ${file}`);
+});
