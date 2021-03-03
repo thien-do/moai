9aabf3139619c3d7dad2121bb882488f71172a0d
Khoa Le <ldakhoa1308@gmail.com>
2021-03-03 21:30:29 +0700

    Docs(core): Improve dialog docs

--- core/src/dialog/dialog.stories.tsx
+++ core/src/dialog/dialog.stories.tsx
@@ -1,11 +1,29 @@
-import { storiesOf } from "@storybook/react";
+import { _Story } from "../_story";
 import { Button } from "../button/button";
 import { DivPx } from "../div/div";
 import { Dialog, DialogPane } from "./dialog";
+import { dialogAlert, dialogConfirm, dialogPrompt } from "../";
 
-storiesOf("Dialog", module).add("Main", () => (
-	<div>
-		<DialogPane width="content">
+const _width = {
+	content: "content",
+	fixed: "fixed",
+};
+
+export default {
+	title: "Components/Dialog",
+	component: Dialog,
+	argTypes: {
+		width: _width,
+	},
+};
+
+interface Props {
+	width: "content" | "fixed";
+}
+
+export const Primary = (props: Props) => {
+	return (
+		<DialogPane width={props.width}>
 			<Dialog.Body>
 				<Dialog.Message
 					children={[
@@ -20,5 +38,50 @@ storiesOf("Dialog", module).add("Main", () => (
 				<Button highlight>Publish</Button>
 			</Dialog.Footer>
 		</DialogPane>
-	</div>
-));
+	);
+};
+
+export const Alert = () => {
+	const alert = () =>
+		dialogAlert([
+			"Cannot save post",
+			"There was a network error. Please check your connection and try again.",
+		]);
+	return <Button onClick={alert} children="Show alert" />;
+};
+
+_Story.desc(Alert)(`
+ Moai's alternative to window.alert
+ See https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+`);
+
+export const Confirm = () => {
+	const confirm = () =>
+		dialogConfirm([
+			"Publish post?",
+			"Published posts can be seen by anyone on the internet.",
+		]);
+	return <Button onClick={confirm} children="Show confirm" />;
+};
+
+_Story.desc(Confirm)(`
+ Moai's alternative to window.confirm
+ See https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+`);
+
+export const Prompt = () => {
+	const prompt = async () => {
+		const title = await dialogPrompt([
+			"Enter post title",
+			"Post title is required to publish",
+		]);
+		if (!title) return;
+		dialogAlert(`Post "${title}" is published!`);
+	};
+	return <Button onClick={prompt} children="Show prompt" />;
+};
+
+_Story.desc(Prompt)(`
+ Moai's alternative to window.prompt
+ See https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+`);
