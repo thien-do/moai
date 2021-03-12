cdd3626a206ff3ab9d08a328196a67dce1710ca3
Thien <dvkndn@gmail.com>
2021-03-12 23:17:27 +0700

    Fix(core): Hide background switcher in Gallery

--- core/src/_gallery/toolbar/toolbar.tsx
+++ core/src/_gallery/toolbar/toolbar.tsx
@@ -1,11 +1,13 @@
 import { border, ThemeSwitcher } from "..";
 import { background } from "../../background/background";
-import { BackgroundSwitcher } from "./background";
+import { DivPx } from "../../div/div";
 import s from "./toolbar.module.css";
 
 export const GalleryToolbar = () => (
 	<div className={[s.container, border.weak, background.strong].join(" ")}>
-		<div children={<ThemeSwitcher />} />
-		<div children={<BackgroundSwitcher />} />
+		<span>Theme:</span>
+		<DivPx size={16} />
+		<ThemeSwitcher />
+		{/* <div children={<BackgroundSwitcher />} /> */}
 	</div>
 );
