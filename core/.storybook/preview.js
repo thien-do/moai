4f66c7bb7b5a408b3f439c51dc1c9cdf83cc1296
Thien <dvkndn@gmail.com>
2021-03-14 00:07:45 +0700

    Docs(core): Extract preview's typographic CSS

--- core/.storybook/preview.js
+++ core/.storybook/preview.js
@@ -2,6 +2,7 @@ import * as D from "@storybook/addon-docs/blocks";
 import { useEffect } from "react";
 import "../font/remote.css";
 import "./preview.css";
+import "./typography.css";
 import "./syntax.css";
 import { storyTheme } from "./theme";
 
