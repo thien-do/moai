4c8b6decd52663d5d675bc4694920cae6395857c
Thien <hello@thien.do>
2021-05-08 22:15:40 +0700

    Fix(core): Fix dead widget gallery

--- core/src/_gallery/feedback/tooltip.tsx
+++ core/src/_gallery/feedback/tooltip.tsx
@@ -3,7 +3,7 @@ import { DivPx, Button, Tooltip, TooltipPane } from "..";
 export const GalleryFeedbackTooltip = (): JSX.Element => (
 	<div>
 		<Tooltip content="Sample Tooltip">
-			<Button.Forwarded children="Hover to show a Tooltip" />
+			<Button children="Hover to show a Tooltip" />
 		</Tooltip>
 		<DivPx size={8} />
 		<TooltipPane children="Short Tooltip" />
