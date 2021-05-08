e046d454ae1b40362727b378d76b2049c759ad6a
Thien <hello@thien.do>
2021-05-08 20:54:10 +0700

    Fix(core): Allow any number as prop of DivPx

--- core/src/div/div.tsx
+++ core/src/div/div.tsx
@@ -3,9 +3,7 @@ import s from "./div.module.css";
 // eslint-disable-next-line no-irregular-whitespace
 export const DivEm = (): JSX.Element => <span> </span>;
 
-export type DivSize = 4 | 8 | 12 | 16 | 20 | 24 | 32 | 48 | 64 | 96;
-
-export const DivPx = ({ size }: { size: DivSize }): JSX.Element => (
+export const DivPx = ({ size }: { size: number }): JSX.Element => (
 	<span className={s.px} style={{ width: size, height: size }} />
 );
 
