dc9d01740dba9b981c388f21bbcbbbcf4bd75989
Thien Do <hello@thien.do>
2021-05-22 21:45:34 +0700

    Fix(docs): Fix wrong styling of Select
    This is because of missing auto-prefix in "docs" project. In "core" project the auto-prefix still runs in rollup, but there are none in "docs".
    
    This PR adds postcss and autoprefixer to "docs".

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,6 +1,18 @@
 /** @type {import("@storybook/core/types/index").StorybookConfig} */
 module.exports = {
 	stories: ["../src/**/*.stories.@(ts|tsx|mdx)"],
-	addons: ["@storybook/addon-docs", "storybook-css-modules-preset"],
+	addons: [
+		"@storybook/addon-docs",
+		{
+			name: "@storybook/addon-postcss",
+			options: {
+				postcssLoaderOptions: {
+					implementation: require("postcss"),
+					postcssOptions: { plugins: [require("autoprefixer")] },
+				},
+				cssLoaderOptions: { modules: { auto: true } },
+			},
+		},
+	],
 	features: { postcss: false },
 };
