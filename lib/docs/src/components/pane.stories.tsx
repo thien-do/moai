b233de2e2b3a27ad6f94b1fc93ba653dee21e68d
Thien Do <hello@thien.do>
2021-05-29 17:18:31 +0700

    Fix(docs): Improve pane docs

--- lib/docs/src/components/pane.stories.tsx
+++ lib/docs/src/components/pane.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta } from "@storybook/react";
 import { Pane } from "../../../core/src";
 import { Utils } from "../utils/utils";
-import { GalleryContainerPane } from "../../../gallery/src/container/pane";
 
 const meta: Meta = {
 	title: "Components/Pane",
@@ -10,12 +9,13 @@ const meta: Meta = {
 		children: Utils.arg("React.ReactNode"),
 		noPadding: Utils.arg("boolean"),
 		fullHeight: Utils.arg("boolean"),
+		contentWidth: Utils.arg("boolean"),
 	},
 };
 
 Utils.page.component(meta, {
 	primary: "sticky",
-	shots: [<GalleryContainerPane key="1" />],
+	shots: [],
 });
 
 export default meta;
@@ -23,34 +23,31 @@ export default meta;
 interface Props {
 	noPadding?: boolean;
 	fullHeight?: boolean;
+	contentWidth?: boolean;
 }
 
 export const Primary = (props: Props): JSX.Element => (
-	<div>
-		<div style={{ height: 100 }}>
-			<Pane noPadding={props.noPadding} fullHeight={props.fullHeight}>
-				<div>FOO</div>
-				<div>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-				</div>
-			</Pane>
-		</div>
+	<div style={{ height: 100 }}>
+		<Pane {...props}>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+		</Pane>
 	</div>
 );
 
-export const Basic = (): JSX.Element => {
-	return (
-		<Pane>
-			<div>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-				Temporibus sapiente vero ad eius fuga atque repellendus?
-				Repellat sint veniam adipisci accusamus, nihil explicabo odio,
-				id neque ducimus voluptate nulla? Maiores?
-			</div>
-		</Pane>
-	);
-};
+export const Basic = (): JSX.Element => (
+	<Pane>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+		sapiente vero ad eius fuga atque repellendus? Repellat sint veniam
+		adipisci accusamus, nihil explicabo odio, id neque ducimus voluptate
+		nulla? Maiores?
+	</Pane>
+);
 
-Utils.story(Basic, { desc: `
-To begin, you only need to provide the content inside pane via children.
-`});
+Utils.story(Basic, {
+	desc: `
+A pane only require its \`children\` to be defined. See the [props table][1]
+below for props to control its padding, width and height.
+
+[1]: #props
+`,
+});
