0805033c0142bb53f25a23c21c17b013dc9ec11e
NgocNhi123 <lnnhi18@apcs.vn>
2021-06-01 14:22:51 +0700

    icon component doc

--- lib/core/src/icon/icon.tsx
+++ lib/core/src/icon/icon.tsx
@@ -4,11 +4,26 @@ import { IconType } from "react-icons";
 export type IconComponent = IconType;
 
 export interface IconProps {
+	/**
+	 * How the icon will be displayed
+	 */
 	display: "block" | "inline";
+	/**
+	 * Which icon will be display (choose one from coreIcons)
+	 */
 	component: IconComponent;
+	/**
+	 * Size of the icon
+	 */
 	size?: number;
 }
 
+/**
+ * Icons are a great tool to visualize and emphasize the content that you want
+ * user's attention. The icon component is used to generate icons that are
+ * defined in coreIcons component or you can define your own icon set with
+ * [react-icons](https://react-icons.github.io/react-icons).
+ */
 export const Icon = (props: IconProps): JSX.Element => {
 	const size = props.size ?? 16;
 	return props.component({
