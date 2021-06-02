4d04920ea288c57b957e6258ec916b9e978b4733
Thien Do <hello@thien.do>
2021-06-02 15:39:59 +0700

    Feat(docs): Add color docs

---
+++ lib/docs/src/color/background/background.tsx
@@ -0,0 +1,48 @@
+import { border, background, Table, text } from "../../../../core/src";
+import { ColorSample } from "../sample/sample";
+import s from "./background.module.css";
+
+type BackgroundKey = keyof typeof background;
+
+interface Row {
+	key: BackgroundKey;
+}
+
+const MakeColumn = (theme: "light" | "dark", text: string) => (
+	row: Row
+): JSX.Element => (
+	<div className={theme}>
+		<ColorSample
+			background={background[row.key]}
+			foreground={{ type: "text", cls: text, usage: "both" }}
+		/>
+	</div>
+);
+
+const LightStrong = MakeColumn("light", text.normal);
+const LightWeak = MakeColumn("light", text.muted);
+const DarkStrong = MakeColumn("dark", text.normal);
+const DarkWeak = MakeColumn("dark", text.muted);
+
+interface Props {
+	rows: Row[];
+}
+
+export const ColorBackground = (props: Props): JSX.Element => (
+	<div className={[s.container, border.weak].join(" ")}>
+		<Table<Row>
+			size={Table.sizes.small}
+			fixed={{ firstColumn: true }}
+			fill
+			rows={props.rows}
+			rowKey={(row) => row.key}
+			columns={[
+				{ title: "Name", className: s.name, render: "key" },
+				{ title: "Light", render: LightStrong },
+				{ title: "Light (muted)", render: LightWeak },
+				{ title: "Dark", render: DarkStrong },
+				{ title: "Dark (muted)", render: DarkWeak },
+			]}
+		/>
+	</div>
+);
