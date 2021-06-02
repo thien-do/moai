12249e7561a2341392fe8de2d75ccccc723ae6fa
NgocNhi123 <lnnhi18@apcs.vn>
2021-06-02 10:01:31 +0700

    rewrite some sentences

--- lib/docs/src/patterns/icon.stories.tsx
+++ lib/docs/src/patterns/icon.stories.tsx
@@ -10,8 +10,8 @@ Utils.page.pattern(meta, {
 	desc: `
 There are several components across moai that have the icon property (like
 [Button](https://docs.moaijs.com/?path=/docs/components-button--primary) and
-[Input](https://docs.moaijs.com/?path=/docs/components-input--primary)). With
-this prop, you can put an icon inside the desired component.
+[Input](https://docs.moaijs.com/?path=/docs/components-input--primary)). You can
+use this prop to display an icon inside desired component.
 `,
 });
 
@@ -20,21 +20,22 @@ export default meta;
 // Is required by Storybook
 export const Primary = (): JSX.Element => <div>Skipped</div>;
 
-export const Usage = (): JSX.Element => (
-	<div style={{ width: "25%" }}>
+export const Basic = (): JSX.Element => (
+	<div style={{ width: "25%", display: "flex" }}>
+		<Input icon={coreIcons.check} placeholder="Foo" />
+		<DivPx size={12} />
 		<Button icon={coreIcons.caret} iconRight={true}>
 			Foo
 		</Button>
-		<DivPx size={12} />
-		<Input icon={coreIcons.check} placeholder="Foo" />
 	</div>
 );
 
-Utils.story(Usage, {
-	name: "Usage",
+Utils.story(Basic, {
+	name: "Basic",
 	desc: `
-To display an icon inside a component (that has icon prop), you can choose one
-from coreIcons component or create your own set of icons by using
+To get started, you need to put an icon that has IconType as the value. There
+are some ways for you to get this icon: you can either choose one from our
+coreIcons component or create your own set of icons by using
 [react-icons](https://react-icons.github.io/react-icons/).
 Thus, some components might have other icon releated props for customization.
 `,
