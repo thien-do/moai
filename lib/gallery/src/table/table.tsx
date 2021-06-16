c8e2c63840249ebe56d0d1bb9bba2989907719b8
NgocNhi123 <lnnhi18@apcs.vn>
2021-06-16 16:49:46 +0700

    update table gallery example

--- lib/gallery/src/table/table.tsx
+++ lib/gallery/src/table/table.tsx
@@ -1,11 +1,11 @@
 import { Fragment, useState } from "react";
 import * as M from "../../../core/src";
-import { Robot, ROBOTS } from "./robots";
+import { Book, BOOKS } from "../example/normalizedBooks";
 import s from "./table.module.css";
 import { GoSearch, GoMail } from "react-icons/go";
 
 interface RowProps {
-	robot: Robot;
+	book: Book;
 }
 
 type Column = (props: RowProps) => JSX.Element;
@@ -18,146 +18,87 @@ const SearchHeader = ({ children }: { children: string }): JSX.Element => (
 	</div>
 );
 
-const Overview: Column = ({ robot }: RowProps) => (
+const Overview: Column = ({ book }: RowProps) => (
 	<div className={s.overview}>
 		<img
 			width="32"
 			height="32"
-			src={robot.avatar}
-			alt={`Image of ${robot.name.last}`}
+			src={book.cover.small}
+			alt={`Image of ${book.title}`}
 			className="block"
-			style={{ background: `${robot.color}20` }}
 		/>
 		<M.DivPx size={8} />
-		<div>
-			{robot.name.first} {robot.name.last}
-		</div>
+		<p>{book.title}</p>
 	</div>
 );
 
-const LastSeenHeader = (): JSX.Element => (
+const PublishDateHeader = (): JSX.Element => (
 	<div>
-		<div>Last seen</div>
+		<div>Publish</div>
 		<M.DivPx size={8} />
 		<M.Input type="date" placeholder="Search" />
 	</div>
 );
 
-const Action: Column = ({ robot }: RowProps) => (
-	<div className={s.actions}>
-		{robot.deployed ? (
-			<M.Button highlight children="Deploy" />
-		) : (
-			<>
-				<M.Button children="Retreat" />
-				<M.DivPx size={8} />
-				<M.Button children="Upgrade" />
-			</>
-		)}
-	</div>
-);
-
-// https://github.com/Marak/faker.js/blob/master/lib/locales/en/commerce/product_name.js#L21
-const MATERIAL_TAGS = {
-	Steel: M.Tag.colors.gray,
-	Wooden: M.Tag.colors.yellow,
-	Concrete: M.Tag.colors.green,
-	Plastic: M.Tag.colors.blue,
-	Cotton: M.Tag.colors.indigo,
-	Granite: M.Tag.colors.purple,
-	Rubber: M.Tag.colors.pink,
-	Metal: M.Tag.colors.red,
-	Soft: M.Tag.colors.gray,
-	Fresh: M.Tag.colors.green,
-	Frozen: M.Tag.colors.blue,
-};
-
-const MaterialsHeader = (): JSX.Element => (
-	<div>
-		<div>Materials</div>
-		<M.DivPx size={8} />
-		<M.Select
-			options={[...Object.keys(MATERIAL_TAGS), "Filter: All"].map(
-				M.Select.toStringOption
-			)}
-			defaultValue="Filter: All"
-		/>
-	</div>
-);
-
-const Materials: Column = ({ robot }: RowProps) => (
-	<div className={s.materials}>
-		{robot.materials.map((material, index) => (
-			<Fragment key={material}>
-				{index > 0 && <M.DivPx size={4} />}
-				<M.Tag
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					color={(MATERIAL_TAGS as any)[material]}
-					children={material}
-				/>
-			</Fragment>
+const Subjects: Column = ({ book }: RowProps) => (
+	<div className={s.subjects}>
+		{book.subjects.map((subject) => (
+			<Fragment key={subject}>{subject},</Fragment>
 		))}
 	</div>
 );
 
-const LastSeen: Column = ({ robot }: RowProps) => (
-	<div>{new Date(robot.lastSeen).toDateString()}</div>
+const PublishDate: Column = ({ book }: RowProps) => (
+	<div>{book.publish_date}</div>
 );
 
-const Note: Column = ({ robot }: RowProps) => (
+const Url: Column = ({ book }: RowProps) => (
 	<M.Paragraph>
-		<span>{robot.note}</span>
-		<span>{` — ${robot.name.last}`}</span>
+		<a>{book.url}</a>
 	</M.Paragraph>
 );
 
-const Mac: Column = ({ robot }: RowProps) => (
-	<div className={s.mac}>
-		<M.Button size={M.Button.sizes.small} icon={GoMail} iconLabel="Mail" />
-		<M.DivPx size={8} />
-		<div>{robot.MAC}</div>
-	</div>
+const Author: Column = ({ book }: RowProps) => (
+	<div className={s.author}>{book.author}</div>
 );
 
-const getColumns = (): M.TableColumn<Robot>[] => [
+const getColumns = (): M.TableColumn<Book>[] => [
 	{
-		title: <SearchHeader children="Bot" />,
-		render: (robot) => <Overview robot={robot} />,
+		title: <SearchHeader children="Title" />,
+		render: (book) => <Overview book={book} />,
 	},
 	{
-		title: <LastSeenHeader />,
-		render: (robot) => <LastSeen robot={robot} />,
+		title: <PublishDateHeader />,
+		render: (book) => <PublishDate book={book} />,
 	},
 	{
-		title: <SearchHeader children="MAC" />,
-		render: (robot) => <Mac robot={robot} />,
+		title: <SearchHeader children="Author" />,
+		render: (book) => <Author book={book} />,
 	},
 	{
-		title: <MaterialsHeader />,
-		render: (robot) => <Materials robot={robot} />,
+		title: <SearchHeader children="Publishers" />,
+		render: "publishers",
 	},
 	{
-		title: <SearchHeader children="Email" />,
-		render: "email",
-	},
-	{
-		title: "Action",
-		render: (robot) => <Action robot={robot} />,
+		title: <SearchHeader children="Subject" />,
+		render: (book) => <Subjects book={book} />,
 	},
 ];
 
 export const GalleryTable = (): JSX.Element => {
 	const [selected, setSelected] = useState<Set<string>>(new Set());
+
 	const table = (
 		<M.Table
-			rows={ROBOTS}
+			rows={BOOKS as Book[]}
 			columns={getColumns()}
-			rowKey={(robot) => robot.id}
-			expandable={{ render: (robot) => <Note robot={robot} /> }}
+			rowKey={(book) => book.key}
+			expandable={{ render: (book) => <Url book={book} /> }}
 			selectable={{ selected, setSelected }}
 			fixed={{ header: true, firstColumn: true }}
 		/>
 	);
+
 	return (
 		<div className={s.wrapper}>
 			<M.Pane noPadding>
