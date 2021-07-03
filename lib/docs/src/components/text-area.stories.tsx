313c6dd882992f813e597bb7c099eeaa3a9921c8
Thien Do <hello@thien.do>
2021-07-03 10:01:26 +0700

    Feat(core): Fix TextArea to extend from built-in props
    Fix #240

--- lib/docs/src/components/text-area.stories.tsx
+++ lib/docs/src/components/text-area.stories.tsx
@@ -7,24 +7,8 @@ export default {
 	argTypes: {
 		style: Utils.arg(TextArea.styles),
 		size: Utils.arg(TextArea.sizes),
-
 		disabled: Utils.arg("boolean"),
 		readOnly: Utils.arg("boolean"),
-		rows: Utils.arg(null),
-
-		defaultValue: Utils.arg(null),
-		value: Utils.arg(null),
-		forwardedRef: Utils.arg(null),
-		setValue: Utils.arg(null),
-		placeholder: Utils.arg(null),
-		autoFocus: Utils.arg(null),
-		autoSelect: Utils.arg(null),
-
-		onBlur: Utils.arg(null),
-		onFocus: Utils.arg(null),
-		onKeyPress: Utils.arg(null),
-		onKeyUp: Utils.arg(null),
-		onKeyDown: Utils.arg(null),
 	},
 };
 
