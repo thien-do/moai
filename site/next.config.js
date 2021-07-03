0a5c2eab5efd4c63c7b3dc36b4787b8d1f0d5d0e
Thien Do <hello@thien.do>
2021-07-04 05:40:57 +0700

    Build: Extract site to another repo

--- site/next.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-
-module.exports = {
-	future: {
-		webpack5: true,
-	},
-};
--- site/pages/tests/empty.tsx
+++
@@ -1,8 +0,0 @@
-const Empty = (): JSX.Element => (
-	<div>
-		This is an empty page. It does not import anything. It is meant to be a
-		based to measure sizes of other pages.
-	</div>
-);
-
-export default Empty;
