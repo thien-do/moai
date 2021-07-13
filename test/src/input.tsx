830ca2109e04fa15eb039a4e43e0201d106162d8
NgocNhi123 <lnnhi18@apcs.vn>
2021-07-13 13:12:02 +0700

    fix(test): resolve change request

--- test/src/input.tsx
+++ test/src/input.tsx
@@ -6,8 +6,8 @@ const InputTesting = () => {
 	const [value, setValue] = useState("");
 	return (
 		<div>
-			<Input value={value} setValue={setValue} data-testid="input" />
-			<div data-testid="div">{value}</div>
+			<Input value={value} setValue={setValue} aria-label="input" />
+			<div>{value}</div>
 			<button data-testid="button" onClick={() => setValue("Goodbye")}>
 				Change
 			</button>
@@ -17,35 +17,23 @@ const InputTesting = () => {
 
 describe("Testing Input Component", () => {
 	test("div value should change after Input value change", async () => {
-		const spy = jest.spyOn(console, "error");
-		spy.mockImplementation(() => void 0);
-
 		render(<InputTesting />);
 
-		const inputElement = screen.getByTestId("input") as HTMLInputElement;
-		const divElement = screen.getByTestId("div");
+		const inputElement = screen.getByLabelText("input") as HTMLInputElement;
 
 		fireEvent.change(inputElement, { target: { value: "Hello" } });
 		expect(inputElement.value).toBe("Hello");
-		expect(divElement.textContent).toBe("Hello");
-
-		spy.mockRestore();
+		expect(screen.getByText("Hello")).toBeDefined();
 	});
 
-	test("Input and div values should change after button clicked", async () => {
-		const spy = jest.spyOn(console, "error");
-		spy.mockImplementation(() => void 0);
-
+	test("Input's text should be changed when props.value is changed", async () => {
 		render(<InputTesting />);
 
-		const inputElement = screen.getByTestId("input") as HTMLInputElement;
-		const divElement = screen.getByTestId("div");
+		const inputElement = screen.getByLabelText("input") as HTMLInputElement;
 		const buttonElement = screen.getByTestId("button");
 
-		fireEvent.click(buttonElement, { bubbles: true });
-		expect(divElement.textContent).toBe("Goodbye");
+		fireEvent.click(buttonElement);
+		expect(screen.getByText("Goodbye")).toBeDefined();
 		expect(inputElement.value).toBe("Goodbye");
-
-		spy.mockRestore();
 	});
 });
