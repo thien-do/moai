0635aa8df899a43884ebff800145c829aa57a116
NgocNhi123 <lnnhi18@apcs.vn>
2021-07-13 12:25:12 +0700

    feat(test): create testing for input component

--- test/src/input.tsx
+++ test/src/input.tsx
@@ -1,27 +1,50 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { renderHook } from "@testing-library/react-hooks";
 import { Input } from "@moai/core";
 import { useState } from "react";
 
-describe("Input", () => {
-	test("throw if input value is not changed", () => {
+const InputTesting = () => {
+	const [value, setValue] = useState("");
+	return (
+		<div>
+			<Input value={value} setValue={setValue} data-testid="input" />
+			<div data-testid="div">{value}</div>
+			<button data-testid="button" onClick={() => setValue("Goodbye")}>
+				Change
+			</button>
+		</div>
+	);
+};
+
+describe("Testing Input Component", () => {
+	test("div value should change after Input value change", async () => {
 		const spy = jest.spyOn(console, "error");
 		spy.mockImplementation(() => void 0);
 
-		const { result } = renderHook(() => useState("Hello"));
-		const [value, setValue] = result.current;
+		render(<InputTesting />);
 
-		render(
-			<Input placeholder="test-input" value={value} setValue={setValue} />
-		);
+		const inputElement = screen.getByTestId("input") as HTMLInputElement;
+		const divElement = screen.getByTestId("div");
 
-		render(<div data-testid="testing">{value}</div>);
-
-		const inputElement = screen.getByPlaceholderText("test-input");
-		const divElement = screen.getByTestId("testing");
-
-		fireEvent.change(inputElement, "Hello");
+		fireEvent.change(inputElement, { target: { value: "Hello" } });
+		expect(inputElement.value).toBe("Hello");
 		expect(divElement.textContent).toBe("Hello");
+
+		spy.mockRestore();
+	});
+
+	test("Input and div values should change after button clicked", async () => {
+		const spy = jest.spyOn(console, "error");
+		spy.mockImplementation(() => void 0);
+
+		render(<InputTesting />);
+
+		const inputElement = screen.getByTestId("input") as HTMLInputElement;
+		const divElement = screen.getByTestId("div");
+		const buttonElement = screen.getByTestId("button");
+
+		fireEvent.click(buttonElement, { bubbles: true });
+		expect(divElement.textContent).toBe("Goodbye");
+		expect(inputElement.value).toBe("Goodbye");
 
 		spy.mockRestore();
 	});
