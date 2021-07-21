2c3cbfd971334ff68c4ea345713de3f3a762cecb
NgocNhi123 <lnnhi18@apcs.vn>
2021-07-21 15:40:47 +0700

    feat(test): create test for Select props

--- test/src/button.tsx
+++ test/src/button.tsx
@@ -1,6 +1,4 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
 import { Button } from "@moai/core";
 import { useState } from "react";
 
