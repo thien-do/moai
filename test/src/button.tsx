532266e7c02ca02f8de473a86bd8adc2d08fe4b8
NgocNhi123 <lnnhi18@apcs.vn>
2021-07-21 11:06:35 +0700

    feat(test): create testing for Button props: onClick, disabled, busy

--- test/src/button.tsx
+++ test/src/button.tsx
@@ -1,5 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import { Button } from "@moai/core";
+import { useState } from "react";
 
 describe("Button", () => {
 	test("throws if has no content", () => {
@@ -9,5 +12,72 @@ describe("Button", () => {
 			render(<Button />);
 		}).toThrowError('must have either "icon" or "children"');
 		spy.mockRestore();
+	});
+});
+
+describe("Testing Button onClick", () => {
+	const defaultValue = "Hello";
+	const newValue = "Goodbye";
+	const TestingButton = () => {
+		const [text, setText] = useState(defaultValue);
+		return (
+			<div>
+				<div>{text}</div>
+				<Button onClick={() => setText(newValue)}>
+					Testing Button
+				</Button>
+			</div>
+		);
+	};
+	test("Div's value should be changed when click button", () => {
+		render(<TestingButton />);
+		const buttonElement = screen.getByText("Testing Button");
+		expect(screen.getByText(defaultValue)).toBeDefined();
+		fireEvent.click(buttonElement);
+		expect(screen.getByText(newValue)).toBeDefined();
+	});
+});
+
+describe("Testing Button Disabled Prop", () => {
+	const defaultValue = "Hello";
+	const newValue = "Goodbye";
+	const TestingButton = () => {
+		const [text, setText] = useState(defaultValue);
+		return (
+			<div>
+				<div>{text}</div>
+				<Button disabled onClick={() => setText(newValue)}>
+					Testing Button
+				</Button>
+			</div>
+		);
+	};
+	test("Div's value shouldn't be changed when click button", () => {
+		render(<TestingButton />);
+		const buttonElement = screen.getByText("Testing Button");
+		fireEvent.click(buttonElement);
+		expect(screen.getByText(defaultValue)).toBeDefined();
+	});
+});
+
+describe("Testing Button Busy Prop", () => {
+	const defaultValue = "Hello";
+	const newValue = "Goodbye";
+	const TestingButton = () => {
+		const [text, setText] = useState(defaultValue);
+		return (
+			<div>
+				<div>{text}</div>
+				<Button busy onClick={() => setText(newValue)}>
+					Testing Button
+				</Button>
+			</div>
+		);
+	};
+	test("Button should be disabled when busy", () => {
+		render(<TestingButton />);
+		const buttonElement = screen.getByText("Testing Button");
+		fireEvent.click(buttonElement);
+		expect(screen.getByText(defaultValue)).toBeDefined();
 	});
 });
