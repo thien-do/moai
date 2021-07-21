a70177661bd767775bd23c2f46d443190d8c7d84
NgocNhi123 <lnnhi18@apcs.vn>
2021-07-21 10:43:57 +0700

    feat(test): create testing for Input prop: type, disabled

--- test/src/input.tsx
+++ test/src/input.tsx
@@ -1,21 +1,26 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import { Input } from "@moai/core";
 import { useState } from "react";
 
-const InputTesting = () => {
-	const [value, setValue] = useState<string>("");
-	return (
-		<div>
-			<Input value={value} setValue={setValue} aria-label="input" />
-			<div>{value}</div>
-			<button data-testid="button" onClick={() => setValue("Goodbye")}>
-				Change
-			</button>
-		</div>
-	);
-};
-
 describe("Testing Input Controlled Props", () => {
+	const InputTesting = () => {
+		const [value, setValue] = useState<string>("");
+		return (
+			<div>
+				<Input value={value} setValue={setValue} aria-label="input" />
+				<div>{value}</div>
+				<button
+					data-testid="button"
+					onClick={() => setValue("Goodbye")}
+				>
+					Change
+				</button>
+			</div>
+		);
+	};
+
 	test("div value should change after Input value change", async () => {
 		render(<InputTesting />);
 
@@ -39,8 +44,12 @@ describe("Testing Input Controlled Props", () => {
 });
 
 describe("Testing Input Uncontrolled Props", () => {
+	const InputTesting = () => (
+		<Input defaultValue="Foo" aria-label="default-input" />
+	);
+
 	test("props.defaultValue should be displayed before Input's text change", async () => {
-		render(<Input defaultValue="Foo" aria-label="default-input" />);
+		render(<InputTesting />);
 
 		const inputElement = screen.getByLabelText(
 			"default-input"
@@ -50,7 +59,7 @@ describe("Testing Input Uncontrolled Props", () => {
 	});
 
 	test("props.defaultValue should be replace when Input's text change", async () => {
-		render(<Input defaultValue="Foo" aria-label="default-input" />);
+		render(<InputTesting />);
 
 		const inputElement = screen.getByLabelText(
 			"default-input"
@@ -58,5 +67,29 @@ describe("Testing Input Uncontrolled Props", () => {
 
 		fireEvent.change(inputElement, { target: { value: "Hello" } });
 		expect(inputElement.value).toBe("Hello");
+	});
+});
+
+describe("Testing Input Type Prop", () => {
+	test("props.type 'number' doesn't allow characters", async () => {
+		const inputValue = "foo";
+		render(<Input type="number" aria-label="input-test" />);
+		const inputElement = screen.getByLabelText(
+			"input-test"
+		) as HTMLInputElement;
+		userEvent.type(inputElement, inputValue);
+		expect(inputElement).not.toHaveValue(inputValue);
+	});
+});
+
+describe("Testing Input Disable Prop", () => {
+	test("props.type 'number' doesn't allow characters", async () => {
+		const inputValue = "foo";
+		render(<Input disabled aria-label="input-test" />);
+		const inputElement = screen.getByLabelText(
+			"input-test"
+		) as HTMLInputElement;
+		userEvent.type(inputElement, inputValue);
+		expect(inputElement).not.toHaveValue(inputValue);
 	});
 });
