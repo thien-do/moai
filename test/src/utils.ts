e60b210aca5e400bc9e112b000c1801230e7781f
Ro Ngoc Vo <ngocro208@gmail.com>
2021-07-28 15:26:55 +0700

    fix(test): fix build

--- test/src/utils.ts
+++ test/src/utils.ts
@@ -3,7 +3,7 @@ import { fireEvent, screen } from "@testing-library/react";
 export const getButton = (name: string): HTMLElement =>
 	screen.getByRole("button", { name });
 
-export const fireEscEvent = (element: HTMLElement) => {
+export const fireEscEvent = (element: HTMLElement): void => {
 	element.focus();
 	fireEvent.keyDown(element, {
 		key: "Escape",
