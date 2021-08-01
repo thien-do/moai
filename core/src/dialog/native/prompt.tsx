d64c0cd827953bd078967c98c6d954a4f11005e1
Thien Do <hi.thiendo@icloud.com>
2021-08-01 16:55:57 +0700

    Fix(core): Remove invalid prop in Dialog.prompt

--- core/src/dialog/native/prompt.tsx
+++ core/src/dialog/native/prompt.tsx
@@ -23,7 +23,6 @@ export const PromptDialog = (props: Props): JSX.Element => {
 		value: text,
 		setValue: setText,
 		autoFocus: true,
-		autoselect: "true",
 	};
 	const rows = props.rows ?? 1;
 	return (
