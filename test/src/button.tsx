2a8079a64343cb3c16a60f4bb27055949d1805a7
Ro Ngoc Vo <33742348+ngocro208@users.noreply.github.com>
2021-08-01 12:34:03 +0700

    Test: Use user-event and jest-dom in Input tests (#268)
    Co-authored-by: Thien Do <hi.thiendo@icloud.com>

--- test/src/button.tsx
+++ test/src/button.tsx
@@ -3,11 +3,9 @@ import { Button } from "@moai/core";
 
 describe("Button", () => {
 	test("throws if has no content", () => {
-		const spy = jest.spyOn(console, "error");
-		spy.mockImplementation(() => void 0);
+		jest.spyOn(console, "error").mockImplementation(() => void 0);
 		expect(() => {
 			render(<Button />);
 		}).toThrowError('must have either "icon" or "children"');
-		spy.mockRestore();
 	});
 });
--- test/src/input.tsx
+++ test/src/input.tsx
@@ -1,62 +1,49 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import { Input } from "@moai/core";
+import { Button, Input } from "@moai/core";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { useState } from "react";
 
-const InputTesting = () => {
-	const [value, setValue] = useState<string>("");
-	return (
-		<div>
-			<Input value={value} setValue={setValue} aria-label="input" />
-			<div>{value}</div>
-			<button data-testid="button" onClick={() => setValue("Goodbye")}>
-				Change
-			</button>
-		</div>
-	);
-};
-
-describe("Testing Input Controlled Props", () => {
-	test("div value should change after Input value change", async () => {
-		render(<InputTesting />);
-
-		const inputElement = screen.getByLabelText("input") as HTMLInputElement;
-
-		fireEvent.change(inputElement, { target: { value: "Hello" } });
-		expect(inputElement.value).toBe("Hello");
-		expect(screen.getByText("Hello")).toBeDefined();
+describe("Input Controlled", () => {
+	const ControlledInput = () => {
+		const [value, setValue] = useState<string>("Pikachu");
+		return (
+			<div>
+				<label htmlFor="name">Name</label>
+				<Input id="name" value={value} setValue={setValue} />
+				<div>Pokemon is {value}</div>
+				<Button onClick={() => setValue("Eevee")}>Change</Button>
+			</div>
+		);
+	};
+	test("Changes in the Input should be reflected outside", () => {
+		render(<ControlledInput />);
+		const input = screen.getByRole("textbox", { name: "Name" });
+		userEvent.clear(input);
+		userEvent.type(input, "Mew");
+		const div = screen.getByText("Pokemon is", { exact: false });
+		expect(div).toHaveTextContent("Pokemon is Mew");
 	});
-
-	test("Input's text should be changed when props.value is changed", async () => {
-		render(<InputTesting />);
-
-		const inputElement = screen.getByLabelText("input") as HTMLInputElement;
-		const buttonElement = screen.getByTestId("button");
-
-		fireEvent.click(buttonElement);
-		expect(screen.getByText("Goodbye")).toBeDefined();
-		expect(inputElement.value).toBe("Goodbye");
+	test("Changes outside should be reflected in the Input", () => {
+		render(<ControlledInput />);
+		const button = screen.getByRole("button", { name: "Change" });
+		userEvent.click(button);
+		const input = screen.getByRole("textbox", { name: "Name" });
+		expect(input).toHaveDisplayValue("Eevee");
 	});
 });
 
-describe("Testing Input Uncontrolled Props", () => {
-	test("props.defaultValue should be displayed before Input's text change", async () => {
-		render(<Input defaultValue="Foo" aria-label="default-input" />);
-
-		const inputElement = screen.getByLabelText(
-			"default-input"
-		) as HTMLInputElement;
-
-		expect(inputElement.value).toBe("Foo");
-	});
-
-	test("props.defaultValue should be replace when Input's text change", async () => {
-		render(<Input defaultValue="Foo" aria-label="default-input" />);
-
-		const inputElement = screen.getByLabelText(
-			"default-input"
-		) as HTMLInputElement;
-
-		fireEvent.change(inputElement, { target: { value: "Hello" } });
-		expect(inputElement.value).toBe("Hello");
+describe("Input Uncontrolled", () => {
+	const UncontrolledInput = () => (
+		<div>
+			<label htmlFor="name">Name</label>
+			<Input id="name" defaultValue="Pikachu" />
+		</div>
+	);
+	test("Direct changes to the Input should work", () => {
+		render(<UncontrolledInput />);
+		const input = screen.getByRole("textbox", { name: "Name" });
+		userEvent.clear(input);
+		userEvent.type(input, "Mew");
+		expect(input).toHaveDisplayValue("Mew");
 	});
 });
