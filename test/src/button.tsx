2077f0f7f048059ecb1d497fbd57e8d3c30a3a17
Thien Do <hi.thiendo@icloud.com>
2021-08-01 13:40:24 +0700

    Test: Improve Button tests

--- test/src/button.tsx
+++ test/src/button.tsx
@@ -1,79 +1,51 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { Button } from "@moai/core";
+import { Button, ButtonProps } from "@moai/core";
 import { useState } from "react";
+import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
-	test("throws if has no content", () => {
+	test("Should throws if has no content", () => {
 		jest.spyOn(console, "error").mockImplementation(() => void 0);
 		expect(() => {
 			render(<Button />);
 		}).toThrowError('must have either "icon" or "children"');
 	});
-});
 
-describe("Testing Button onClick", () => {
-	const defaultValue = "Hello";
-	const newValue = "Goodbye";
-	const TestingButton = () => {
-		const [text, setText] = useState(defaultValue);
+	const defaultName = "Pikachu";
+	const newName = "Eevee";
+	const Test = ({ button }: { button: ButtonProps }) => {
+		const [name, setName] = useState(defaultName);
 		return (
 			<div>
-				<div>{text}</div>
-				<Button onClick={() => setText(newValue)}>
-					Testing Button
+				<div>Name is {name}</div>
+				<Button onClick={() => setName(newName)} {...button}>
+					Change
 				</Button>
 			</div>
 		);
 	};
-	test("Div's value should be changed when click button", () => {
-		render(<TestingButton />);
-		const buttonElement = screen.getByText("Testing Button");
-		expect(screen.getByText(defaultValue)).toBeDefined();
-		fireEvent.click(buttonElement);
-		expect(screen.getByText(newValue)).toBeDefined();
+
+	test("Should trigger `onClick` handler", () => {
+		render(<Test button={{}} />);
+		const button = screen.getByRole("button", { name: "Change" });
+		userEvent.click(button);
+		const div = screen.getByText("Name is", { exact: false });
+		expect(div).toHaveTextContent("Name is Eevee");
 	});
-});
-
-describe("Testing Button Disabled Prop", () => {
-	const defaultValue = "Hello";
-	const newValue = "Goodbye";
-	const TestingButton = () => {
-		const [text, setText] = useState(defaultValue);
-		return (
-			<div>
-				<div>{text}</div>
-				<Button disabled onClick={() => setText(newValue)}>
-					Testing Button
-				</Button>
-			</div>
-		);
-	};
-	test("Div's value shouldn't be changed when click button", () => {
-		render(<TestingButton />);
-		const buttonElement = screen.getByText("Testing Button");
-		fireEvent.click(buttonElement);
-		expect(screen.getByText(defaultValue)).toBeDefined();
+	test("Should not trigger `onClick` when `disabled` is set", () => {
+		render(<Test button={{ disabled: true }} />);
+		const button = screen.getByRole("button", { name: "Change" });
+		expect(button).toBeDisabled();
+		userEvent.click(button);
+		const div = screen.getByText("Name is", { exact: false });
+		expect(div).toHaveTextContent("Name is Pikachu");
 	});
-});
-
-describe("Testing Button Busy Prop", () => {
-	const defaultValue = "Hello";
-	const newValue = "Goodbye";
-	const TestingButton = () => {
-		const [text, setText] = useState(defaultValue);
-		return (
-			<div>
-				<div>{text}</div>
-				<Button busy onClick={() => setText(newValue)}>
-					Testing Button
-				</Button>
-			</div>
-		);
-	};
-	test("Button should be disabled when busy", () => {
-		render(<TestingButton />);
-		const buttonElement = screen.getByText("Testing Button");
-		fireEvent.click(buttonElement);
-		expect(screen.getByText(defaultValue)).toBeDefined();
+	test("Should be disabled when `busy` is set", () => {
+		render(<Test button={{ busy: true }} />);
+		const button = screen.getByRole("button", { name: "Change" });
+		expect(button).toBeDisabled();
+		userEvent.click(button);
+		const div = screen.getByText("Name is", { exact: false });
+		expect(div).toHaveTextContent("Name is Pikachu");
 	});
 });
