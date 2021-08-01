f276d65a5071813a31da262e3135c617dfe39462
Thien Do <hi.thiendo@icloud.com>
2021-08-01 13:46:08 +0700

    Chore(test): Fix code style

--- test/src/button.tsx
+++ test/src/button.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Button, ButtonProps } from "@moai/core";
 import { useState } from "react";
 import userEvent from "@testing-library/user-event";
