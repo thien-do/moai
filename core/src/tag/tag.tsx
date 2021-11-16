7435ff11329781ad1e9ac6f7b44020ba241b20cd
Ro Ngoc Vo <ngocro208@gmail.com>
2021-11-16 18:03:44 +0700

    Chore(tag): Support forwardedRef and size

--- core/src/tag/tag.tsx
+++ core/src/tag/tag.tsx
@@ -1,3 +1,4 @@
+import { ForwardedRef } from "react";
 import { border } from "../border/border";
 import { CategoryColor, categoryColors } from "../category/category";
 import s from "./tag.module.css";
@@ -15,6 +16,17 @@ interface Props {
 	 * Color of the tag. Use one from `Tag.colors`.
 	 */
 	color: CategoryColor;
+	/**
+	 * Size of the tag. Use one from `Tag.sizes`.
+	 */
+	size?: string;
+	/**
+	 * A [reference][1] to the underlying `span` element. Usually useful in
+	 * uncontrolled mode.
+	 *
+	 * [1]: https://reactjs.org/docs/forwarding-refs.html
+	 */
+	forwardedRef?: ForwardedRef<HTMLSpanElement>;
 }
 
 /**
@@ -23,11 +35,21 @@ interface Props {
  *
  * [1]: https://developer.mozilla.org/en-US/docs/Web/HTML/Inline_elements
  */
-export const Tag = ({ children, color }: Props): JSX.Element => (
+export const Tag = ({
+	children,
+	color,
+	size,
+	forwardedRef,
+}: Props): JSX.Element => (
 	<span
-		className={[border.radius, s.container, color].join(" ")}
+		className={[border.radius, s.container, color, size].join(" ")}
 		children={children}
+		ref={forwardedRef}
 	/>
 );
 
 Tag.colors = categoryColors;
+Tag.sizes = {
+	medium: s.mediumTag,
+	small: s.smallTag,
+};
