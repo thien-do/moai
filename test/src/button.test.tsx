94be34b5cf921a8cc34137f7a0423bd19a6144e4
Ro Ngoc Vo <ngocro208@gmail.com>
2021-11-16 17:20:23 +0700

    Chore(button): Button should pass onClick to its "a" render and support "rel"

--- test/src/button.test.tsx
+++ test/src/button.test.tsx
@@ -13,13 +13,14 @@ describe("Button", () => {
 
 	const defaultName = "Pikachu";
 	const newName = "Eevee";
+	const buttonLabel = "Change";
 	const Test = ({ button }: { button: ButtonProps }) => {
 		const [name, setName] = useState(defaultName);
 		return (
 			<div>
 				<div>Name is {name}</div>
 				<Button onClick={() => setName(newName)} {...button}>
-					Change
+					{buttonLabel}
 				</Button>
 			</div>
 		);
@@ -27,14 +28,14 @@ describe("Button", () => {
 
 	test("Should trigger `onClick` handler", () => {
 		render(<Test button={{}} />);
-		const button = screen.getByRole("button", { name: "Change" });
+		const button = screen.getByRole("button", { name: buttonLabel });
 		userEvent.click(button);
 		const div = screen.getByText("Name is", { exact: false });
 		expect(div).toHaveTextContent("Name is Eevee");
 	});
 	test("Should not trigger `onClick` when `disabled` is set", () => {
 		render(<Test button={{ disabled: true }} />);
-		const button = screen.getByRole("button", { name: "Change" });
+		const button = screen.getByRole("button", { name: buttonLabel });
 		expect(button).toBeDisabled();
 		userEvent.click(button);
 		const div = screen.getByText("Name is", { exact: false });
@@ -42,10 +43,42 @@ describe("Button", () => {
 	});
 	test("Should be disabled when `busy` is set", () => {
 		render(<Test button={{ busy: true }} />);
-		const button = screen.getByRole("button", { name: "Change" });
+		const button = screen.getByRole("button", { name: buttonLabel });
 		expect(button).toBeDisabled();
 		userEvent.click(button);
 		const div = screen.getByText("Name is", { exact: false });
 		expect(div).toHaveTextContent("Name is Pikachu");
+	});
+
+	describe("Render an a if `href` is provided", () => {
+		test("Renders correctly", () => {
+			render(
+				<Test
+					button={{ href: "https://moaijs.com/", rel: "noopener" }}
+				/>
+			);
+
+			expect(
+				screen.queryByRole("button", { name: buttonLabel })
+			).not.toBeInTheDocument();
+			const link = screen.getByRole("link", { name: buttonLabel });
+			expect(link).toHaveAttribute("rel", "noopener");
+		});
+
+		test("Should trigger `onClick` event", () => {
+			const onClickMockFn = jest.fn();
+			render(
+				<Test
+					button={{
+						href: "https://moaijs.com/",
+						rel: "noopener",
+						onClick: onClickMockFn,
+					}}
+				/>
+			);
+			const link = screen.getByRole("link", { name: buttonLabel });
+			userEvent.click(link);
+			expect(onClickMockFn).toHaveBeenCalledTimes(1);
+		});
 	});
 });
