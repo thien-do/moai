28a1ebcc5ec01be0a27b6424bd4707021e4677bc
clitetailor <ducnhatphung@gmail.com>
2021-11-20 13:46:30 +0700

    Feat(core): Add Blank Icon

---
+++ core/src/icons/blank-icon.tsx
@@ -0,0 +1,7 @@
+import { GenIcon, IconBaseProps, IconType } from "react-icons";
+
+export const BlankIcon: IconType = (props: IconBaseProps): JSX.Element => {
+	return GenIcon({ tag: "svg", attr: { viewBox: "0 0 16 16" }, child: [] })(
+		props
+	);
+};
--- core/src/icons/icons.tsx
+++ core/src/icons/icons.tsx
@@ -1,11 +1,13 @@
 import * as go from "react-icons/go";
 import * as ri from "react-icons/ri";
+import { BlankIcon } from "./blank-icon";
 
 /**
  * A very small subset of the "icon" package. This contains icons that are used
  * by the "core" package to avoid importing "icon" into "core"
  */
 export const coreIcons = {
+	blank: BlankIcon,
 	caret: go.GoTriangleDown,
 	check: go.GoCheck,
 	chevronDown: go.GoChevronDown,
