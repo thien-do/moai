4d13a2fae494170521638287b8b81a193cfb1987
NgocNhi123 <ngocnhi@166>
2024-06-28 16:28:17 +0700

    update yarn version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import jestDom from "eslint-plugin-jest-dom";
 import tsParser from "@typescript-eslint/parser";
+import jestDom from "eslint-plugin-jest-dom";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,6 +23,7 @@ export default [
 			"**/public",
 			"test/coverage",
 			"**/docs/.storybook",
+			"**/new-docs/.storybook",
 		],
 	},
 	...fixupConfigRules(
