c8b4678210183531031d07576ca2dcc7ce244206
NgocNhi123 <ngocnhi@166>
2024-06-30 23:02:03 +0700

    migrate draft components

---
+++ new-docs/src/components/menu.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta } from "@storybook/react";
+import { Menu } from "../../../core/src";
+
+export default {
+	title: "Draft/Menu",
+	component: Menu,
+} as Meta;
+
+export const Primary = (): JSX.Element => (
+	<Menu
+		items={[
+			{ label: "Foo", fn: () => window.alert("foo") },
+			{ label: "Bar", fn: () => window.alert("bar") },
+			{ label: "Baz", disabled: true },
+		]}
+	/>
+);
