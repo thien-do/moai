adb6f4849e4bd0b0357a5d760caf968a762794b4
Thien Do <5953369+thien-do@users.noreply.github.com>
2024-07-23 22:59:54 +0700

    fix

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,40 +1,39 @@
 import { DocsContainer, Unstyled } from "@storybook/blocks";
-import { Preview } from '@storybook/react';
+import { Preview } from "@storybook/react";
 import { useDarkMode } from "storybook-dark-mode";
-import { themes } from './theme';
+import { themes } from "./theme";
 
-import "../global.css";
 import "./preview.css";
 import "./syntax.css";
 import "./typography.css";
 
 const Container: typeof DocsContainer = (props) => {
-	const dark = useDarkMode();
-	const theme = dark ? themes.dark : themes.light
-	return (
-		<DocsContainer {...props} theme={theme}>
-			<Unstyled>{props.children}</Unstyled>
-		</DocsContainer>
-	);
+  const dark = useDarkMode();
+  const theme = dark ? themes.dark : themes.light;
+  return (
+    <DocsContainer {...props} theme={theme}>
+      <Unstyled>{props.children}</Unstyled>
+    </DocsContainer>
+  );
 };
 
 const preview: Preview = {
-	parameters: {
-		docs: {
-			container: Container,
-		},
-		options: {
-			storySort: {
-				method: "alphabetical",
-				order: [
-					"Intro",
-					["Quick Start", "Proper Start"],
-					"Patterns",
-					"Components",
-				],
-			},
-		},
-	},
+  parameters: {
+    docs: {
+      container: Container,
+    },
+    options: {
+      storySort: {
+        method: "alphabetical",
+        order: [
+          "Intro",
+          ["Quick Start", "Proper Start"],
+          "Patterns",
+          "Components",
+        ],
+      },
+    },
+  },
 };
 
 export default preview;
