f01a00a6f28e70be0b6fd2adfb1979839845c8db
NgocNhi123 <ngocnhi@166>
2025-04-26 16:11:34 +0900

    duplicate draft docs from old docs to new docs

---
+++ src/docs/components/button-group.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta } from "@storybook/react";
+import { GoSearch } from "react-icons/go";
+import { Button, ButtonGroup, Input, Select } from "../../../core/src";
+
+export default {
+  title: "Draft/Button Group",
+  component: ButtonGroup,
+} as Meta;
+
+export const Primary = (): JSX.Element => {
+  const input = <Input placeholder="Search" />;
+  const button = <Button icon={GoSearch} iconLabel="Search" />;
+  const select = <Select options={["Posts"].map(Select.toStringOption)} />;
+  return (
+    <div style={{ width: 320 }}>
+      <ButtonGroup>
+        {[
+          { fill: false, element: select },
+          { fill: true, element: input },
+          { fill: false, element: button },
+        ]}
+      </ButtonGroup>
+    </div>
+  );
+};
